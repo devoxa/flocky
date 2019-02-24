eaf8c5d93693b5cc268a5afa676327322a7c3a9f
queicherius <queicherius@gmail.com>
2019-02-24 11:35:00 +0000

    Add scripts for publishing (2)

--- publish.sh
+++ publish.sh
@@ -1,14 +1,45 @@
 #!/usr/bin/env bash
 set -e
 
+if [[ "$1" == "" ]];
+then
+  echo "Missing version argument:"
+  echo "./publish.sh --patch"
+  echo "./publish.sh --minor"
+  echo "./publish.sh --major"
+  exit 1
+fi
+
+echo "Verifying the current branch is 'master'"
+BRANCH=$(git symbolic-ref --short HEAD)
+if [[ "$BRANCH" != "master" ]];
+then
+  echo "Not on 'master' branch."
+  exit 1
+fi
+
+echo "Verifying the current branch is clean"
+CHANGES=$(git status --porcelain)
+if [[ "$CHANGES" != "" ]];
+then
+  echo "Unclean working tree. Commit or stash changes first."
+  exit 1
+fi
+
 echo "Running tests"
 yarn test
 
-echo "Updating readme"
+echo "Updating README"
 yarn generate-docs
 
 echo "Building package"
 yarn build
 
+echo "Bumping the package version"
+yarn version "$1"
+
 echo "Publishing package"
-./node_modules/.bin/np --yolo --contents build/
+yarn publish build/
+
+echo "Pushing to Github"
+git push

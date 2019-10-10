f29744ca278710d6316098e73e2feb45d77e2c5d
queicherius <queicherius@gmail.com>
2019-10-10 17:47:35 +0100

    [compact] Add new function for compacting arrays with falsy values

---
+++ src/compact/index.ts
@@ -0,0 +1,18 @@
+/**
+ * ### compact(array)
+ *
+ * Create an array with all falsy (`undefined`, `null`, `false`, `0`, `NaN`, `''`) values removed.
+ *
+ * ```js
+ * flocky.compact([1, 2, 3, null, 4, false, 0, NaN, 5])
+ * // -> [1, 2, 3, 4, 5]
+ * ```
+ */
+
+type Falsy = undefined | null | false | 0 | ''
+
+function compact<T>(array: Array<T | Falsy>): Array<T> {
+  return array.filter(Boolean) as Array<T>
+}
+
+export = compact

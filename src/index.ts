2f75296d64d305ded235cbf908b6d8233e0265dd
queicherius <queicherius@gmail.com>
2019-11-14 15:05:44 +0000

    Better export from index file

--- src/index.ts
+++ src/index.ts
@@ -19,6 +19,27 @@ import { slugify } from './slugify'
 import { sum } from './sum'
 import { unique } from './unique'
 
+export { average } from './average'
+export { chunk } from './chunk'
+export { clone } from './clone'
+export { compact } from './compact'
+export { get } from './get'
+export { hash } from './hash'
+export { identifier } from './identifier'
+export { max } from './max'
+export { min } from './min'
+export { omit } from './omit'
+export { pick } from './pick'
+export { random } from './random'
+export { randomString } from './randomString'
+export { roundTo } from './roundTo'
+export { sample } from './sample'
+export { shuffle } from './shuffle'
+export { sleep } from './sleep'
+export { slugify } from './slugify'
+export { sum } from './sum'
+export { unique } from './unique'
+
 export default {
   average,
   chunk,

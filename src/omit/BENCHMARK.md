9744700524ec3682bcfaa431105bfcd7d05e29fb
queicherius <queicherius@gmail.com>
2020-09-14 01:17:34 +0000

    Benchmark `omit` function

---
+++ src/omit/BENCHMARK.md
@@ -0,0 +1,11 @@
+### Benchmark for `omit`
+
+[Source for this benchmark](./benchmark.ts)
+
+|     | flocky |
+| --- | --- |
+| 5 properties / 3 omitted | **3,840,844 ops/sec (100.00%)** |
+| 26 properties / 3 omitted | **282,094 ops/sec (100.00%)** |
+| 10 properties / 55 omitted | **765,157 ops/sec (100.00%)** |
+
+<sup>Generated at 2020-09-14 with Node.JS v14.4.0</sup>
---
+++ src/omit/benchmark.ts
@@ -0,0 +1,76 @@
+import Benchmark from '../benchmarkHelper'
+import { omit } from './index'
+import { randomString } from '../randomString'
+
+let benchmark = new Benchmark('omit')
+
+let omitKeys: Array<string> = []
+for (let i = 0; i !== 55; i++) {
+  omitKeys.push(randomString(10))
+}
+
+benchmark.add({
+  library: 'flocky',
+  input: '5 properties / 3 omitted',
+  func: () => omit({ a: 1, b: 2, c: 3, d: 4, e: 5 }, ['a', 'b', 'd']),
+})
+
+benchmark.add({
+  library: 'flocky',
+  input: '26 properties / 3 omitted',
+  func: () => {
+    const object = {
+      a: 1,
+      b: 2,
+      c: 3,
+      d: 4,
+      e: 5,
+      f: 6,
+      g: 7,
+      h: 8,
+      i: 9,
+      j: 10,
+      k: 11,
+      l: 12,
+      m: 13,
+      n: 14,
+      o: 15,
+      p: 16,
+      q: 17,
+      r: 18,
+      s: 19,
+      t: 20,
+      u: 21,
+      v: 22,
+      w: 23,
+      x: 24,
+      y: 25,
+      z: 26,
+    }
+
+    return omit(object, ['a', 'b', 'd'])
+  },
+})
+
+benchmark.add({
+  library: 'flocky',
+  input: '10 properties / 55 omitted',
+  func: () => {
+    const object = {
+      a: 1,
+      b: 2,
+      c: 3,
+      d: 4,
+      e: 5,
+      f: 6,
+      g: 7,
+      h: 8,
+      i: 9,
+      j: 10,
+    }
+
+    return omit(object, omitKeys as any)
+  },
+})
+
+benchmark.run()
